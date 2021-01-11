c3731269f5d7775812282f22755efd44bc686aa6
maskertech <maskertech@gmail.com>
2021-01-11 17:48:59 +0700

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ffc287bc31c9117cb6d65f2/1erofs95k';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 (function () {
   const doc = document
   const rootEl = doc.documentElement
